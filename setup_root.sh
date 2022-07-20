01e5d5ad5ec60df7beb8b177bb04f9be06c73a5d
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-07-20 11:31:05 +0200

    fix routing among leafs

--- setup_root.sh
+++ setup_root.sh
@@ -17,7 +17,7 @@ for leaf in $leafs; do
 	leaf_private=$(go run main.go $(cat private) $leaf)
 	leaf_public=$(echo $leaf_private | wg pubkey)
 	leaf_wg_ip=$(bash string_to_ip.sh "$leaf")
-	# wg set $device peer $leaf_public persistent-keepalive 10 allowed-ips $leaf_ip
-	wg set $device peer $leaf_public persistent-keepalive 10 allowed-ips 10.0.0.0/8
+	wg set $device peer $leaf_public persistent-keepalive 10 allowed-ips $leaf_wg_ip
+	# wg set $device peer $leaf_public persistent-keepalive 10 allowed-ips 10.0.0.0/8
 done
 
