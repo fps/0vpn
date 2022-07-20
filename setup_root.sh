68a956ec76d04b769c6d34b89ef2c12504dd42aa
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-07-20 10:35:21 +0200

    add root setup script

---
+++ setup_root.sh
@@ -0,0 +1,20 @@
+set -x
+root_hostname=contabo
+root_port=4242
+root_wg_ip=$(bash string_to_ip.sh $root_hostname)/8
+leafs="nixos physalis"
+device=wg0
+
+ip link add $device type wireguard
+ip addr add $root_wg_ip dev $device
+go run main.go $(cat private) $root_hostname private_derived
+wg set $device private-key private_derived
+wg set $device listen-port $root_port
+ip link set $device up
+
+for leaf in $leafs; do
+	leaf_private=$(go run main.go $(cat private) $leaf)
+	leaf_public=$(echo $leaf_private | wg pubkey)
+	leaf_ip=$(bash string_to_ip.sh "$leaf")
+	wg set $device peer $leaf_public allowed-ips $leaf_ip
+done
