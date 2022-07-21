0a781eed996630500509b7d4e19981844ee994ca
FPS <mista.tapas@gmx.net>
2022-07-21 13:41:32 +0200

    go fmt

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	"os"
-	"fmt"
 	"crypto/ed25519"
 	"encoding/base64"
+	"fmt"
+	"os"
 )
 
 func MixinString(masterKey ed25519.PrivateKey, hostname string) ed25519.PrivateKey {
-    sigPrivSlice := masterKey[0:32]
-    for index := 0; index < len(sigPrivSlice); index++ {
-        sigPrivSlice[index] = sigPrivSlice[index] ^ hostname[index % len(hostname)]
-    }
-    return ed25519.NewKeyFromSeed(sigPrivSlice)
+	sigPrivSlice := masterKey[0:32]
+	for index := 0; index < len(sigPrivSlice); index++ {
+		sigPrivSlice[index] = sigPrivSlice[index] ^ hostname[index%len(hostname)]
+	}
+	return ed25519.NewKeyFromSeed(sigPrivSlice)
 }
-
 
 func main() {
 	if len(os.Args) < 4 {
@@ -26,24 +25,24 @@ func main() {
 		fmt.Println("   derive a new key by mixing in string")
 		fmt.Println("   into key and output the new key to stdout")
 		fmt.Println("")
-		fmt.Println("resolve [string]")
+		fmt.Println("resolve [network] [string]")
 		fmt.Println("   hash [string] into an IP address")
+		fmt.Println("   and output the IP address to stdout")
+		fmt.Println("   [network] example: 10.10.0.0/16")
 		os.Exit(1)
 	}
 
 	if os.Args[1] == "mixin" {
-    	input_key_bytes := make([]byte, 32)
-    	_, err := base64.StdEncoding.Decode(input_key_bytes, []byte(os.Args[2]))
-    	if (err != nil) {
-    		fmt.Println("Failed to decode key")
-            os.Exit(1)
-    	}
-    	input_key := ed25519.PrivateKey(input_key_bytes)
-    
-    	mixed_key := MixinString(input_key, os.Args[3])
-    
-    	fmt.Println(base64.StdEncoding.EncodeToString(mixed_key[0:32]))
-    }
+		input_key_bytes := make([]byte, 32)
+		_, err := base64.StdEncoding.Decode(input_key_bytes, []byte(os.Args[2]))
+		if err != nil {
+			fmt.Println("Failed to decode key")
+			os.Exit(1)
+		}
+		input_key := ed25519.PrivateKey(input_key_bytes)
+
+		mixed_key := MixinString(input_key, os.Args[3])
+
+		fmt.Println(base64.StdEncoding.EncodeToString(mixed_key[0:32]))
+	}
 }
-
-
