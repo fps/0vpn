a1511ece3f5a1b15ad0ecf0a7cce0dcc4fe0cc65
fps <mista.tapas@gmx.net>
2022-08-04 09:14:02 +0200

    add a command to derive a key from a passphrase

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module fps.io/0vpn-tool
 
 go 1.17
 
-
+require golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"crypto/ed25519"
+	"golang.org/x/crypto/scrypt"
 	"encoding/base64"
 	"fmt"
 	"os"
+	"bufio"
 )
 
 func MixinString(masterKey ed25519.PrivateKey, hostname string) ed25519.PrivateKey {
@@ -15,27 +17,35 @@ func MixinString(masterKey ed25519.PrivateKey, hostname string) ed25519.PrivateK
 	return ed25519.NewKeyFromSeed(sigPrivSlice)
 }
 
+func print_usage() {
+	fmt.Println("Usage: 0vpn command [arguments]")
+	fmt.Println("")
+	fmt.Println("where command can be one of the following:")
+	fmt.Println("")
+	fmt.Println("help")
+	fmt.Println("    show this help text")
+	fmt.Println("")
+	fmt.Println("mixin [key] [string]")
+	fmt.Println("   Derive a new key by mixing in string")
+	fmt.Println("   into key and output the new key to stdout")
+	fmt.Println("")
+	fmt.Println("key-from-password")
+	fmt.Println("   Derive a key from a password given at")
+	fmt.Println("   stdin and output the new key to stdout")
+}
+
 func main() {
-	if len(os.Args) < 4 || os.Args[1] == "help" {
-		fmt.Println("Usage: 0vpn command [arguments]")
-		fmt.Println("")
-		fmt.Println("where command can be one of the following:")
-		fmt.Println("")
-		fmt.Println("help")
-		fmt.Println("    show this help text")
-		fmt.Println("")
-		fmt.Println("mixin [key] [string]")
-		fmt.Println("   Derive a new key by mixing in string")
-		fmt.Println("   into key and output the new key to stdout")
-		fmt.Println("")
+	if len(os.Args) < 2 || os.Args[1] == "help" {
+		print_usage()
 		os.Exit(1)
 	}
 
-	if os.Args[1] == "mixin" {
+	if os.Args[1] == "mixin" && len(os.Args) == 4 {
 		input_key_bytes := make([]byte, 32)
 		_, err := base64.StdEncoding.Decode(input_key_bytes, []byte(os.Args[2]))
 		if err != nil {
 			fmt.Println("Failed to decode key")
+			fmt.Println(err)
 			os.Exit(1)
 		}
 		input_key := ed25519.PrivateKey(input_key_bytes)
@@ -43,5 +53,20 @@ func main() {
 		mixed_key := MixinString(input_key, os.Args[3])
 
 		fmt.Println(base64.StdEncoding.EncodeToString(mixed_key[0:32]))
+		os.Exit(0)
 	}
+	if os.Args[1] == "key-from-password" && len(os.Args) == 2 {
+		line, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		key, err := scrypt.Key([]byte(line), nil, 32768, 8, 1, 32)
+		if err != nil {
+			fmt.Println("Failed to derive key")
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(base64.StdEncoding.EncodeToString(key))
+		os.Exit(0)
+	}
+
+	print_usage()
+	os.Exit(1)
 }
