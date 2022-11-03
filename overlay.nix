c2e15849a7b36e84329758fe62e842dcd1c732f3
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-11-03 09:46:55 +0100

    add vendor directory

--- overlay.nix
+++ overlay.nix
@@ -1,4 +1,18 @@
 final: prev: 
 {
+  zerovpn = prev.buildGoModule rec {
+    pname = "zerovpn";
+    src = ./.;
+    vendorHash = null;
+    name = "zerovpn";
 
+    # GOPATH="${out}/gopath";
+    # GOCACHE="${out}/gocache";
+
+    configurePhase = ''
+      GOPATH=$TMPDIR/gopath
+      GOCACHE=$TMPDIR/gocache
+      go mod vendor
+    '';
+  };
 }
